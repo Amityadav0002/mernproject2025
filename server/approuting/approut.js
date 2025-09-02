d8d33aa48866bf53acb1f2e064712b039a89ece4
Amityadav0002 <amity0397@gmail.com>
2025-09-02 23:46:25 +0530

    use data base

---
+++ server/approuting/approut.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const myapp = express.Router();
+const myschema = require("../schemas/basicinfo");
+
+myapp.get("/",(req,res)=>{
+    res.send("this is my default response sdfsdfsdf000000000000000000");
+});
+
+myapp.get("/about",(req,res)=>{
+    res.send("this is about page");
+});
+
+myapp.get("/allworker", async (req,res)=>{
+    const alldata = await myschema.find();
+    res.send({alldatalist:alldata,status:250,msg:"all worker list"});
+})
+
+
+module.exports = myapp
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const myapp = express();
+const myrouting = require('./approuting/approut');
+require("dotenv").config();
+require("./database/mydb");
+const port = process.env.PORT || 7500
+
+
+
+
+myapp.use(express.json());
+myapp.use(myrouting);
+
+
+
+myapp.listen(port,()=>{
+    console.log(`server is runnning at port no:${port}`);
+})
